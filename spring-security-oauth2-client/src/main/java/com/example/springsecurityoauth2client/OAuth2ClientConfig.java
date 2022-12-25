301860b3197636aef3ac8686c2b0d0ca731679e7
kimjisu <jisu3316@naver.com>
2022-12-25 22:06:29 +0900

    init

---
+++ spring-security-oauth2-client/src/main/java/com/example/springsecurityoauth2client/OAuth2ClientConfig.java
@@ -0,0 +1,24 @@
+package com.example.springsecurityoauth2client;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OAuth2ClientConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authRequest -> authRequest
+                        .antMatchers("/home").permitAll()
+                        .anyRequest().authenticated());
+        http
+//                .oauth2Login();
+                .oauth2Client(Customizer.withDefaults());
+        http.logout().logoutSuccessUrl("/home");
+        return http.build();
+    }
+}
