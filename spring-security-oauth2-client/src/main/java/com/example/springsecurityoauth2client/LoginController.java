33d49f274880923eda237202990e59282cc9b967
kimjisu <jisu3316@naver.com>
2023-01-01 23:33:58 +0900

    OAuth 2.0 Client oauth2Login

--- spring-security-oauth2-client/src/main/java/com/example/springsecurityoauth2client/LoginController.java
+++ spring-security-oauth2-client/src/main/java/com/example/springsecurityoauth2client/LoginController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
-    @GetMapping("/CustomOAuth2AuthorizationRequestsResolver")
-    public String home() {
-        return "CustomOAuth2AuthorizationRequestsResolver";
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
     }
 }
