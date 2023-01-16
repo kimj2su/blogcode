da6272d715bc5cd85853d3c76bdb6ec935490a76
kimjisu <jisu3316@naver.com>
2023-01-16 23:21:06 +0900

    ReSource Server - 검증 기초

--- spring-security-oauh2-resource-server/src/main/java/com/example/springsecurityoauh2resourceserver/SpringSecurityOauth2ResourceServerApplication.java
+++ spring-security-oauh2-resource-server/src/main/java/com/example/springsecurityoauh2resourceserver/SpringSecurityOauth2ResourceServerApplication.java
@@ -3,11 +3,20 @@ package com.example.springsecurityoauh2resourceserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static com.example.springsecurityoauh2resourceserver.jcajce.MacTest.hmac;
+import static com.example.springsecurityoauh2resourceserver.jcajce.MessageDigestTest.messageDigest;
+import static com.example.springsecurityoauh2resourceserver.jcajce.RSATest.rsa;
+import static com.example.springsecurityoauh2resourceserver.jcajce.SignatureTest.signature;
+
 @SpringBootApplication
 public class SpringSecurityOauth2ResourceServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityOauth2ResourceServerApplication.class, args);
+    public static void main(String[] args) throws Exception {
+//        messageDigest("Spring Security");
+//        signature("Spring Security");
+        hmac("Spring Security");
+//        rsa("Spring Security");
+//        SpringApplication.run(SpringSecurityOauth2ResourceServerApplication.class, args);
     }
 
 }
